91f7f4b0e8ce895dab3dbd23d29c92f38568aa42
unknown <subrama2@mail.uc.edu>
2023-09-16 22:20:07 -0400

    lab 3

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,21 @@
+﻿function getIP{
+(Get-NetIPAddress).IPv4Address | Select-String "192*"
+
+}
+function getVersion{
+$Host.Version.Major
+}
+function getUserType{
+Get-LocalUser | Select-String "Admin*"
+}
+
+
+
+$IP = getIP
+$Version = getVersion
+$HostName = hostname
+$Date = Get-Date -DisplayHint Date
+$Type = getUserType
+
+$Body = "This machine's IP is $IP. User is $Type. HostName is $HostName. PowerShell Version $Version. Today's Date is Tuesday, $Date."
+$Body | Out-File C:\Lab3_Output
