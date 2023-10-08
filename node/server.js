1de88f231cdd0377be63a2292737ca54fa2cf4b6
subrama2 <84252650+subrama2@users.noreply.github.com>
2023-10-08 12:52:50 -0400

    Finished lab 6

--- node/server.js
+++ node/server.js
@@ -3,6 +3,23 @@ var fs = require("fs");
 var os = require("os");
 var ip = require('ip');
 
+var totalMembyte = os.totalmem();
+var totalmemoryMB = totalMembyte / 1024 / 1024;
+
+var freememoryByte = os.freemem()
+var freememoryMB = freememoryByte / 1024 / 1024;
+
+var uptimeInSeconds = os.uptime()
+var days = Math.floor(uptimeInSeconds / (24 * 60 * 60))
+var remSecAfterDays = uptimeInSeconds % (24 * 60 * 60)
+var hours = Math.floor(remSecAfterDays / (60 * 60))
+var remSecAfterHours = remSecAfterDays % (60 * 60)
+var minutes = Math.floor(remSecAfterHours / (60))
+var seconds = remSecAfterHours % 60
+
+var cpuinfo = os.cpus()
+var numofcpu = cpuinfo.length;
+
 http.createServer(function(req, res){
 
     if (req.url === "/") {
@@ -22,10 +39,10 @@ http.createServer(function(req, res){
           <body>
             <p>Hostname: ${myHostName}</p>
             <p>IP: ${ip.address()}</p>
-            <p>Server Uptime: </p>
-            <p>Total Memory: </p>
-            <p>Free Memory: </p>
-            <p>Number of CPUs: </p>            
+            <p>Server Uptime: Days: ${days},Hours: ${hours},Minutes: ${minutes},Seconds: ${seconds.toFixed(1)} </p>
+            <p>Total Memory: ${totalmemoryMB.toFixed(3)} MB</p>
+            <p>Free Memory: ${freememoryMB.toFixed(2)} MB </p>
+            <p>Number of CPUs: ${numofcpu}</p>            
           </body>
         </html>` 
         res.writeHead(200, {"Content-Type": "text/html"});
