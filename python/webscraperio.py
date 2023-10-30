60c9eee115f7299175962108a14c57248723a344
unknown <subrama2@mail.uc.edu>
2023-10-30 19:35:16 -0400

    more practice with lab 8

--- python/webscraperio.py
+++ python/webscraperio.py
@@ -1,16 +1,11 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-r = requests.get("http://webscraper.io/test-sites/e-commerce/allinone/phones").content
-soup = BeautifulSoup(r, "lxml")
+data = requests.get("http://www.reebok.com/us/flexagon-energy-shoes---preschool/DV8354.html").content
+soup = BeautifulSoup(data, "html.parser")
+span = soup.find("h1", {"class": "product_information_title___2rG9M product_title gl-heading gl-heading--m"})
+title = span.text
 
-tags = soup.findall('a', {"href":re.compile("[<>#%|\{\}!\\^~\[\]`/]")})
-for a in tags:
-	print(a.get("href"))
-
-r2 = requests.get("http://webscraper.io/test-sites/e-commerce/allinone/phones").content  
-soup2 = BeautifulSoup(r, "lxml")  
-tags2 = soup.findAll("div", {"class":re.compile('(ratings)')})  
-for p in tags2:  
-        a = p.findAll("p",{"class":"pull-right"})  
-        print(a[0].string)  
+span = soup.find("span", {"class": "gl-price__value gl-price__value--sale"})
+price = span.text
+print("Item %s has price %s" % (title,price))
